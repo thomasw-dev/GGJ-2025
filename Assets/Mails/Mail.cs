0cb993912187d163632c630b37d42e23d2856446
thomasw-dev <thomaswang112@gmail.com>
2025-01-25 18:37:30 +0800

    WIP weird bubble-mail collisions

--- Assets/Mails/Mail.cs
+++ Assets/Mails/Mail.cs
@@ -7,8 +7,21 @@ public class Mail : MonoBehaviour
     public MailType.Colors Color = MailType.Colors.None;
     public Vector2 InitialForce = Vector2.zero;
 
+    Rigidbody2D rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
     void FixedUpdate()
     {
         transform.Translate(InitialForce);
+    }
+
+    public void DisableInitialPhysics()
+    {
+        InitialForce = Vector2.zero;
+        rigidBody.bodyType = RigidbodyType2D.Static;
     }
 }
--- Assets/SoapBubble/SoapBubble.cs
+++ Assets/SoapBubble/SoapBubble.cs
@@ -7,6 +7,8 @@ public class SoapBubble : MonoBehaviour
     public float lifeTime = 7;
     public Rigidbody2D rigidBody;
 
+    GameObject _capturedMail;
+
     public void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -18,6 +20,23 @@ public class SoapBubble : MonoBehaviour
         if (lifeTime < 0)
         {
             Destroy(this.gameObject);
+        }
+
+        // Set the mail position to be in the center of itself
+        if (_capturedMail != null)
+        {
+            _capturedMail.transform.SetPositionAndRotation(transform.position, transform.rotation);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.TryGetComponent(out Mail mail))
+        {
+            // Set itself as the parent for the mail
+            mail.transform.SetParent(transform);
+            mail.DisableInitialPhysics();
+            _capturedMail = mail.gameObject;
         }
     }
 }
