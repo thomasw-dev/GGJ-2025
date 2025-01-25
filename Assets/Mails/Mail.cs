15d8e6e4c2a9106608ffc873bf624e22575545ff
Joshua Jenner <joshua-jenner@hotmail.com>
2025-01-25 18:51:52 +0800

    Added Mail Sprites

--- Assets/Mails/Mail.cs
+++ Assets/Mails/Mail.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class Mail : MonoBehaviour
 {
+    public Sprite spriteRed;
+    public Sprite spriteBlue;
+    public Sprite spriteYellow;
+    public SpriteRenderer spriteRenderer;
+
     public MailType.Colors Color = MailType.Colors.None;
     public Vector2 InitialForce = Vector2.zero;
 
     Rigidbody2D rigidBody;
     CircleCollider2D col;
+
+    private void Start()
+    {
+        SetSprite();
+    }
+
+    private void SetSprite()
+    {
+        switch (Color)
+        {
+            case MailType.Colors.Yellow:
+                spriteRenderer.sprite = spriteYellow;
+                break;
+            case MailType.Colors.Red:
+                spriteRenderer.sprite = spriteRed;
+                break;
+            case MailType.Colors.Blue:
+                spriteRenderer.sprite = spriteBlue;
+                break;
+            default:
+                break;
+        }
+    }
 
     void Awake()
     {
