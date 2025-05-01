13cbd6e97b7be6f3b1c1093b4af8468555d396d4
Thomas Wang <thomaswang112@gmail.com>
2025-05-01 22:34:48 +0800

    Touch input hint overlay

--- Assets/Settings/Settings_TouchInput/TouchInputToggler.cs
+++ Assets/Settings/Settings_TouchInput/TouchInputToggler.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class TouchInputToggler : MonoBehaviour
 {
-    [SerializeField] Toggle toggle;
     [SerializeField] GameObject touchInput;
+    [SerializeField] GameObject hintOverlay;
+    bool showHintOverlayWhenUnpaused = false;
 
     void Update()
     {
-        if (toggle != null && touchInput != null)
+        if (showHintOverlayWhenUnpaused && !Global.isGamePaused)
         {
-            bool value = toggle.isOn;
-            Global.useTouchInput = value;
-            touchInput.SetActive(value);
+            hintOverlay.SetActive(true);
+            showHintOverlayWhenUnpaused = false;
         }
+
+        // Clicking anywhere will hide the hint overlay
+        if (hintOverlay.activeSelf && Input.GetMouseButtonDown(0))
+        {
+            hintOverlay.SetActive(false);
+            showHintOverlayWhenUnpaused = false;
+        }
+    }
+
+    public void OnToggleChanged(Toggle toggle)
+    {
+        bool value = toggle.isOn;
+        Global.useTouchInput = value;
+        touchInput.SetActive(value);
+
+        // Turning on touch input will show the hint overlay
+        showHintOverlayWhenUnpaused = value == true;
     }
 }
